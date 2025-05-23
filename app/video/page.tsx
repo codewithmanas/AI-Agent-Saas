88b735850a00eda230ea608ecce5ac4ff5a2e61c
Manas Ranjan Adha <manas865873@gmail.com>
2025-05-23 10:07:11 +0530

    implement shadcn and clerk auth

---
+++ app/video/page.tsx
@@ -0,0 +1,13 @@
+// import { auth } from "@clerk/nextjs/server";
+import React from "react";
+
+const VideoPage = () => {
+    // auth.protect();
+  return (
+    <div>
+      <h1>This is video page.</h1>
+    </div>
+  );
+};
+
+export default VideoPage;
